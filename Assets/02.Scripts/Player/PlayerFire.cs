517917c2d6d8bf3d4e41a0c827ea564eeac172df
박철규 <qcr374343@gmail.com>
2026-09-02 15:22:28 +0900

    feat: 총알 발사 쿨타임 추가

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 //- 생성 취리
 public class PlayerFire : MonoBehaviour
 {
+    public bool CanFire = true;
+    public float TimerTime;
+    private float time;
+    
     public GameObject BulletPrefab;
     public GameObject BulletSubPrefab;
     public Transform FirePoint1;
     public Transform FirePoint2;
     public Transform FirePointSub1;
     public Transform FirePointSub2;
+
     private void Start()
     {
-        
+        time = TimerTime;
     }
-
+    
     private void Update()
     {
+        FireBullet();
+    }
+
+    private void FireBullet()
+    {
         //1. 스페이스바 누름
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && CanFire)
         {
             //2. 총알 프리팹 생성
             //Instantiate는 프리팹을 복사해서 게임 오브젝트를 생성하고 씬에 넣어주는 기능
@@ -27,6 +37,14 @@ public class PlayerFire : MonoBehaviour
             Instantiate(BulletPrefab, FirePoint2.position, FirePoint2.rotation);
             Instantiate(BulletSubPrefab, FirePointSub1.position, FirePointSub1.rotation);
             Instantiate(BulletSubPrefab, FirePointSub2.position, FirePointSub2.rotation);
+            CanFire = false;
+            TimerTime = time;
+        }
+        
+        TimerTime -= Time.deltaTime;
+        if (TimerTime <= 0)
+        {
+            CanFire = true;
         }
     }
 }
