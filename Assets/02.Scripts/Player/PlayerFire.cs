f160c06f3e5be1e49f7af4ab0aa0a591d6adbc36
박철규 <qcr374343@gmail.com>
2026-09-02 15:35:32 +0900

    feat: 공격 모드 토글

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
+
 //space를 누를 때마다 총알을 생성 후 발사
 //필요 속성
 //- 총알 프리팹
 //- 생성 취리
 public class PlayerFire : MonoBehaviour
 {
-    public bool CanFire = true;
+    private bool CanFire = true;
     public float TimerTime;
     private float time;
+    private bool IsManualAttack = true;
     
     public GameObject BulletPrefab;
     public GameObject BulletSubPrefab;
@@ -23,13 +26,33 @@ public class PlayerFire : MonoBehaviour
     
     private void Update()
     {
+        AttackModeToggle();
         FireBullet();
     }
+
+    private void AttackModeToggle()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            IsManualAttack = !IsManualAttack;
+        }
+    } 
 
     private void FireBullet()
     {
         //1. 스페이스바 누름
-        if (Input.GetKeyDown(KeyCode.Space) && CanFire)
+        if (!IsManualAttack && CanFire)
+        {
+            //2. 총알 프리팹 생성
+            //Instantiate는 프리팹을 복사해서 게임 오브젝트를 생성하고 씬에 넣어주는 기능
+            Instantiate(BulletPrefab, FirePoint1.position, FirePoint1.rotation);
+            Instantiate(BulletPrefab, FirePoint2.position, FirePoint2.rotation);
+            Instantiate(BulletSubPrefab, FirePointSub1.position, FirePointSub1.rotation);
+            Instantiate(BulletSubPrefab, FirePointSub2.position, FirePointSub2.rotation);
+            CanFire = false;
+            TimerTime = time;
+        }
+        else if(IsManualAttack && Input.GetKeyDown(KeyCode.Space) && CanFire)
         {
             //2. 총알 프리팹 생성
             //Instantiate는 프리팹을 복사해서 게임 오브젝트를 생성하고 씬에 넣어주는 기능
