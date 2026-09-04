1cf809da7ad9340082bbdfb57a1c4b5c371a1be2
박철규 <qcr374343@gmail.com>
2026-09-04 15:15:10 +0900

    fix: 필요없는 코드 삭제

--- Assets/02.Scripts/Bullet/Bullet.cs
+++ Assets/02.Scripts/Bullet/Bullet.cs
@@ -21,34 +21,6 @@ public class Bullet : MonoBehaviour
     }
 
     //충돌 관련 이벤트 (Enter -> Stay -> Exit)
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("충돌 발생");
-        //Bullet 파괴
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            //GetComponent<타입>(): 게임오브젝트가 가지고 있는 컴포넌트를 참조
-            Enemy enemy = collision.gameObject.GetComponent<Enemy>();
-            //<enemy>는 클래스 말하는거
-
-            //응집도는 높히고, 결합도 낮춰라
-            //결합도는 자주 묻는거
-            //얘는 응집도가 낮아서 별로
-            //enemy것들은 Enemy 클래스에 갖다두자
-            /*
-            enemy.health -= damage;
-            Debug.Log($"{enemy.health}");
-            if (enemy.health <= 0)
-            {
-                //충돌한 게임오브젝트파괴
-                Destroy(collision.gameObject);
-            }
-            */
-            enemy.TakeDamage(damage);
-            Destroy(this.gameObject);
-        }
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("충돌 발생");
