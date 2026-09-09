6c1abe84aa3d9ac4336ba1ee4059f14629315cad
박철규 <qcr374343@gmail.com>
2026-09-09 14:34:36 +0900

    feat: 배경 추가

---
+++ Assets/02.Scripts/Environment/BackgroundScroll.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    private Material _material;
+    [SerializeField] private float _scrollSpeed = 0.1f;
+    private float _offsetY = 0f;
+
+    private void Awake()
+    {
+        _material = GetComponent<SpriteRenderer>().material;
+    }
+
+    private void Update()
+    {
+        _offsetY += _scrollSpeed * Time.deltaTime;
+
+        //ToDo: 머티리얼 프로퍼티 블록을 통한 최적화
+        _material.mainTextureOffset = new Vector2(0, _offsetY);
+    }
+}
